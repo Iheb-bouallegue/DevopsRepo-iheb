7301fcbc775596ab09c19e890174850753f39fe2
mhassini <mourad.hassini@esprit.tn>
2024-11-04 17:19:19 +0100

    front dockerfile

--- src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/universite")
+@CrossOrigin("*")
 public class UniversiteRestController {
 
     IUniversiteService universiteService;
